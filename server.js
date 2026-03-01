3a66797ce7ce232d384b0d9c655dca77cc416c95
MandaVijayKumar <mandavijaykumar40@gmail.com>
2026-03-01 18:05:57 +0530

    third commit

--- server.js
+++ server.js
@@ -18,11 +18,21 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 /* ================= DATABASE CONNECTION ================= */
 
-const db = mysql.createPool({
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.getConnection((err, connection) => {

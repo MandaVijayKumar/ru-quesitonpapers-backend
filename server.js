18ee78e43aed638d44349f22211b2dbfb0b67871
MandaVijayKumar <96509400+MandaVijayKumar@users.noreply.github.com>
2026-03-03 11:49:17 +0530

    Add timezone configuration to database connection

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+    timezone: "+05:30",
   ssl: {
     rejectUnauthorized: false
   }
